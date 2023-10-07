355fc26767ee02bdbe5088912e3c57212b911766
Abdulrahman Amr <abdulrahmanamr@Abdulrahmans-MacBook-Pro.local>
2023-10-07 14:00:17 +0300

    Database and sprint 1 functions nedded for prescriptoins

---
+++ backend /Server/Routes/PrescriptionRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const PrescriptionController = require('../Controllers/PrescriptionController');
+
+// Define routes for Prescription
+router.post('/', PrescriptionController.createPrescription);
+router.get('/', PrescriptionController.getAllPrescriptions);
+router.put('/:id', PrescriptionController.updatePrescription);
+router.delete('/:id', PrescriptionController.deletePrescription);
+router.get('/:patientID', PrescriptionController.getPrescriptionwithPatientID);
+
+module.exports = router;
