0e5a65cf2a558b3cf4e29e274680b33841e57cc2
Abdulrahman Amr <abdulrahmanamr@Abdulrahmans-MacBook-Pro.local>
2023-10-10 19:31:00 +0300

    updated ftont end

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const PrescriptionRoutes = require('./backend /Server/Routes/PrescriptionRoutes.js');
-
+const cors = require('cors');
 const app = express();
+
+
+
+
+
+const allowedOrigins = ['http://localhost:3000'];
+
+// Configure CORS with options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+// Use the cors middleware with options
+app.use(cors(corsOptions));
+
+
+
+
+
 
 app.use(express.json());
 
@@ -20,6 +45,17 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
+
+
+
+
+
+
+
+
+
+
 
 // Start the server on port
 const PORT = 2002;
